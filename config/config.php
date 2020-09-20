f0d34f1f43bf1f195398ce44475dcc262a074d86
evendevil66 <50394633+evendevil66@users.noreply.github.com>
2020-09-21 00:08:41 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,11 +4,11 @@ return [
 
     'name' => "淘乐饭", //产品名称 会反应在用户交互等场景
     'url' => "https://fanli.mttgo.com", //站点url
-    'dtkAppKey' => "5f61c53050d71", //大淘客appKey 使用大淘客接口快速解析商品信息
-    'dtkAppSecret' => "db82913bfba901fce31c89b264adabcf", //大淘客AppSecret
-    'aliAppKey' => "28177009", //淘宝联盟AppKey
-    'aliAppSecret' => "c6078ce94ea6e7f96445a0e1d0b0a8bd", //淘宝联盟AppSecret
-    'pubpid' => 'mm_129401632_1120100319_109804650455' //公用PID
+    'dtkAppKey' => "***", //大淘客appKey 使用大淘客接口快速解析商品信息
+    'dtkAppSecret' => "***", //大淘客AppSecret
+    'aliAppKey' => "***", //淘宝联盟AppKey
+    'aliAppSecret' => "***", //淘宝联盟AppSecret
+    'pubpid' => '***' //公用PID
 ];
 
 
