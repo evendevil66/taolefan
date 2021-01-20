91ce897fd5990cc920bdbac67b8e92a77ff78754
zhangjiaqi <evendevil66@gmail.com>
2021-01-20 23:47:18 +0800

    添加项目介绍

--- app/Http/Controllers/TaokeController.php
+++ app/Http/Controllers/TaokeController.php
@@ -2,13 +2,10 @@
 
 
 namespace App\Http\Controllers;
-use App\Http\Controllers\WeChatController;
-use App\Models\Users;
+use App\Http\Controllers\UserController;
 use TopClient;
 use TbkItemInfoGetRequest;
-use TopAuthTokenCreateRequest;
 use Log;
-use TbkScPublisherInfoSaveRequest;
 
 class TaokeController extends Controller
 {
@@ -49,14 +46,13 @@ class TaokeController extends Controller
     }
 
     /***
-     * @param $user  传入提交转链用户的信息
+     * @param $openid  传入提交转链用户的微信open
      * @param $content 传入用户的完整消息
      * @return 返回转链后的文本信息
      */
-    public function parse($user,$content)
+    public function parse($openid,$content)
     {
-
-        //$user = app(UserController::class) -> getUser($openid);
+        $user = app(UserController::class) -> getUser($openid);
         $rate = 0.8; //后期数据从$user用户对象中获取
         $host = "https://openapi.dataoke.com/api/tb-service/parse-taokouling";
         $data = [
@@ -66,30 +62,18 @@ class TaokeController extends Controller
         ];
         $data['sign'] = $this->makeSign($data);
         $url = $host .'?'. http_build_query($data);
-        //var_dump($url);
+        var_dump($url);
         //处理大淘客解析请求url
         $output = $this->curlGet($url,'get');
         //调用统一请求函数
 
         $dataArr = json_decode($output, true);//将返回数据转为数组
-
         $status = $dataArr['code'];
-        Log::info($dataArr);
-        Log::info($status);
         switch ($status){
             case "0":
                 $goodsid = $dataArr['data']['goodsId'];
-                $dataArr = null;
-                if($user->special_id !=null && $user->special_id !=""){
-                    Log::info("进入带有会员id的转换");
-                    Log::info($user->special_id);
-                    $dataArr = $this->privilegeLinkBySpecialId($goodsid,$user->special_id);
-                }else{
-                    Log::info("进入不带会员id的转换");
-                    $dataArr = $this->privilegeLink($goodsid);
-                }
-
-                Log::info($dataArr);
+                $dataArr = $this->privilegeLink($goodsid);
+                Log::info($dataArr['code']);
                 if ($dataArr['code'] == '0') {
                     $tbArr = $this->aliParse($goodsid);
                     $title = $tbArr['results']['n_tbk_item'][0]['title']; //商品标题
@@ -110,50 +94,32 @@ class TaokeController extends Controller
                     //$start= (strpos($longTpwd,"【"));
                     //$end= (strpos($longTpwd,"】"));
                     //$title= substr($longTpwd,$start+1,$end-$start-1);
-                    $maxCommissionRate = $dataArr['data']['maxCommissionRate']==""||null?$dataArr['data']['minCommissionRate']:$dataArr['data']['maxCommissionRate']; //佣金比例
-                    if($user->special_id !=null && $user->special_id !=""){
-                        return
-                            "1".$title . "\n".
-                            "售价：" . $price . "元\n".
-                            "优惠券：" . $couponInfo . "\n".
-                            "预计付款金额：" . $estimate . "元\n".
-                            "商品返现比例：" . $maxCommissionRate*0.8 . "%\n". //用户返现比例为0.8 后续将从用户表中获取
-                            "预计返现金额：" . ($estimate * $rate * ($maxCommissionRate / 100)) . "元\n".
-                            "返现计算：实付款 * " . $maxCommissionRate*0.8 . "%\n\n".
-                            "复制" . $tpwd . "打开淘宝下单后将订单号发送至公众号即可绑定返现\n\n".
-                            "您已绑定过淘宝账号，下单后系统将尝试自动跟单，如1小时后仍查询不到，您可以手动绑定订单。";
-                    }else{
-                        return
-                            "1".$title . "\n".
-                            "售价：" . $price . "元\n".
-                            "优惠券：" . $couponInfo . "\n".
-                            "预计付款金额：" . $estimate . "元\n".
-                            "商品返现比例：" . $maxCommissionRate*0.8 . "%\n". //用户返现比例为0.8 后续将从用户表中获取
-                            "预计返现金额：" . ($estimate * $rate * ($maxCommissionRate / 100)) . "元\n".
-                            "返现计算：实付款 * " . $maxCommissionRate*0.8 . "%\n\n".
-                            "复制" . $tpwd . "打开淘宝下单后将订单号发送至公众号即可绑定返现\n\n".
-                            "点击下方账号管理，绑定淘宝账号，下单后系统将支持自动同步，无需回传订单号（个别情况自动同步未成功可提交订单号手动绑定）";
-                    }
-
-
+                    $maxCommissionRate = $dataArr['data']['maxCommissionRate']; //佣金比例
+                    return
+                        "1".$title . "\n".
+                        "售价：" . $price . "元\n".
+                        "优惠券：" . $couponInfo . "\n".
+                        "预计付款金额：" . $estimate . "元\n".
+                        "用户返现比例：" . ($rate * 100) . "%\n".
+                        "商品返现比例：" . $maxCommissionRate . "%\n".
+                        "预计返现金额：" . ($estimate * $rate * ($maxCommissionRate / 100)) . "元\n".
+                        "计算公式：" . $estimate . " * " . ($rate * 100) . "% * " . $maxCommissionRate . "%\n".
+                        "最终返现以实际情况为准，绑定订单后可查询到实际返现金额，如出现比价情况，官方会自动对返现金额降低，绑定后如对返现金额不满意可退款。\n".
+                        "复制" . $tpwd . "打开淘宝下单后将订单号发送至公众号即可绑定返现";
                 }else {
                     return "出现未知异常，请稍后再试或联系客服000";
                 }
             case "-1":
                 return "哎呀，服务器出错了，请您再发送尝试一次或稍后再试";
                 break;
-            case "20002" || "200001":
-                return "您发送的信息解析失败，可能不是有效口令，请检查";
-                break;
-            case "20001" || "200003":
-                return "您发送的信息解析失败，可能是商品无任何饭粒活动";
-                break;
+            case "20001":
+                return "您发送的淘口令商品暂时不参与任何返利活动哦";
+            case "20002":
+                return "您发送的信息解析失败\n您可以发送【关键词】获取可解析信息的列表";
             case "25003":
                 return "券信息解析失败，请确保您发送的链接为商品链接，如链接中包含优惠券（如导购群链接等）请先进入商品再分享链接到公众号转链（注意：不要领取第三方淘礼金否则无法返利）";
-                break;
             default:
                 return "出现未知异常，请稍后再试或联系客服";
-                break;
         }
 
     }
@@ -163,33 +129,23 @@ class TaokeController extends Controller
      */
     public function privilegeLink($goodsid){
         $host = "https://openapi.dataoke.com/api/tb-service/get-privilege-link";
-        $data = [
-            'appKey' => config('config.dtkAppKey'),
-            'version' => '1.3.1',
-            'goodsId'=>$goodsid,
-            'pid'=>config('config.pubpid')
-        ];
-        $data['sign'] = $this->makeSign($data);
-        $url = $host .'?'. http_build_query($data);
-        var_dump($url);
-        //处理大淘客解析请求url
-        $output = $this->curlGet($url,'get');
-        $data = json_decode($output, true);//将返回数据转为数组
-        return $data;
-    }
 
-    /**
-     * @param $goodsid 传入预转链的商品id
-     */
-    public function privilegeLinkBySpecialId($goodsid,$specialId){
-        $host = "https://openapi.dataoke.com/api/tb-service/get-privilege-link";
         $data = [
             'appKey' => config('config.dtkAppKey'),
-            'version' => '1.3.1',
+            'version' => '1.2.0',
             'goodsId'=>$goodsid,
-            'pid'=>config('config.pubpid'),
-            'specialId'=>$specialId
         ];
+        $channelId = config('config.relationId');
+        if($channelId != 0){
+            $data = [
+                'appKey' => config('config.dtkAppKey'),
+                'version' => '1.2.0',
+                'goodsId'=>$goodsid,
+                'channelId'=> $channelId
+
+            ];
+        }
+
         $data['sign'] = $this->makeSign($data);
         $url = $host .'?'. http_build_query($data);
         var_dump($url);
@@ -215,73 +171,6 @@ class TaokeController extends Controller
         $Jsondata= json_encode($resp, true);
         $data  = json_decode($Jsondata, true);
         return $data;
-    }
-
-    /**
-     * 通过用户授权获得的code换取sessionid
-     * @param $code
-     * @return mixed 返回处理结果
-     */
-    public function getUserSessionId($code){
-        try{
-        $c = new TopClient;
-        $c->appkey = config('config.aliAppKey');
-        $c->secretKey = config('config.aliAppSecret');
-        $c->format = "json";
-        $req = new TopAuthTokenCreateRequest;
-        $req->setCode($code);
-        $resp = $c->execute($req);
-        $Jsondata= json_encode($resp, true);
-        $data  = json_decode($Jsondata, true);
-        $data = json_decode($data['token_result'],true);
-        return $data['access_token'];
-        }catch (\Exception $e){
-            return false;
-        }
-    }
-
-
-    /***
-     * 绑定会员返回会员id
-     * @param $openid
-     * @param $code
-     */
-
-    public function regMember($openid,$code){
-        $sessionKey=$this->getUserSessionId($code);
-        if($sessionKey == false){
-            return "<script >alert('绑定出错，请联系客服处理！')</script><h1>绑定出错，请联系客服处理！</h1>";
-        }
-        try {
-            $c = new TopClient;
-            $c->appkey = config('config.aliAppKey');
-            $c->secretKey = config('config.aliAppSecret');
-            $c->format = "json";
-            $req = new TbkScPublisherInfoSaveRequest;
-            $req->setInviterCode(config('config.inviter_code'));
-            $req->setInfoType("1");
-            $req->setNote($openid);
-            $resp = $c->execute($req, $sessionKey);
-            $Jsondata= json_encode($resp, true);
-            $data  = json_decode($Jsondata, true);
-            Log::info($data);
-            if($data['data']['special_id']!=null){
-                $special_id = $data['data']['special_id'];
-                $flag = app(Users::class)->updateSpecial_id($openid,$special_id);
-                if($flag==1){
-                    return "<script >alert('绑定成功，您的会员ID为".$special_id."')</script><h1>绑定成功，您的会员ID为".$special_id."</h1>";
-                }else{
-                    return "<script >alert('绑定成功但保存失败，您的会员ID为".$special_id."。您可以联系重试或联系客服提供该ID进行处理')</script><h1>绑定成功但保存失败，您的会员ID为".$special_id."。您可以联系重试或联系客服提供该ID进行处理</h1>";
-                }
-
-            }else{
-                return "<script >alert('绑定出错，请联系客服处理！')</script><h1>绑定出错，请联系客服处理！</h1>";
-            }
-        }catch (\Exception $e){
-            return "<script >alert('绑定出错，请联系客服处理！')</script><h1>绑定出错，请联系客服处理！</h1>";
-        }
-
-
     }
 
 
--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -1,71 +1,10 @@
 <?php
-
 namespace App\Http\Controllers;
-
-use EasyWeChat\OfficialAccount\Application;
 use Log;
 use App\Http\Controllers\TaokeController;
-use App\Models\Users;
 
 class WeChatController extends Controller
 {
-    protected $buttons = [
-        [
-            "name" => "使用教程",
-            "sub_button" => [
-                [
-                    "type" => "click",
-                    "name" => "使用教程",
-                    "key" => "Course"
-                ],
-                [
-                    "type" => "click",
-                    "name" => "联系客服",
-                    "key" => "Contact"
-                ],
-            ],
-        ],
-        [
-            "name" => "饭粒中心",
-            "sub_button" => [
-                [
-                    "type" => "click",
-                    "name" => "订单查询",
-                    "key" => "Order"
-                ],
-                [
-                    "type" => "click",
-                    "name" => "余额查询",
-                    "key" => "Price"
-                ],
-                [
-                    "type" => "click",
-                    "name" => "提现",
-                    "key" => "Receive"
-                ],
-            ],
-        ],
-        [
-            "name" => "个人中心",
-            "sub_button" => [
-                [
-                    "type" => "click",
-                    "name" => "资料补全",
-                    "key" => "Supplement"
-                ],
-                [
-                    "type" => "click",
-                    "name" => "手动注册",
-                    "key" => "Manual"
-                ],
-                [
-                    "type" => "click",
-                    "name" => "绑定淘宝",
-                    "key" => "Bind"
-                ],
-            ],
-        ],
-    ];
 
     /**
      * 处理微信的请求消息
@@ -75,101 +14,53 @@ class WeChatController extends Controller
     public function serve()
     {
         Log::info('request arrived.'); # 日志组建调用自Laravel，非EasyWechat
+
         $app = app('wechat.official_account');
         $app->server->push(function ($message) {
             $openid = $message['FromUserName'];
             //访问数据库获取openid对应用户信息并返回用户对象
-            $user = app(Users::class)->getUserById($openid);
             switch ($message['MsgType']) {
                 case 'event':
-                    if ($message['Event'] == 'subscribe') {
-                        if ($user == null) {
-                            $reg = app(Users::class)->userRegistration($openid);
-                            if ($reg == 1) {
-                                return "欢迎关注" . config('config.name') . "，已为您自动注册成功。" . "\n建议您点击下方个人中心-资料补全，更新资料后开始省钱之旅";
-                            } else {
-                                return "欢迎关注" . config('config.name') . "您的自动注册请求失败，可能系统出现异常，您可以点击下方个人中心-快速注册进行手动注册";
-                            }
-                        } else {
-                            return "欢迎回到" . config('config.name') . "，已为您恢复账号。" . "\n您可以点击下方个人中心-资料补全，确保资料完整即可开始省钱之旅";
-                        }
+                    if($message['Event'] == 'subscribe'){
+                        $url = config('config.apiUrl')."/reg/".$openid;
+                        return "欢迎关注".config('config.name')."，<a href=\"".$url."\">请点此进行注册</a>";
                     }
-                    switch ($message['EventKey']) {
-                        case 'Course':
-                            return
-                                "您可以直接将想购买的商品淘口令发送到这里，系统即可自动转链哦，复制转链后的淘口令打开淘宝继续购物，即可享受优惠和饭粒啦！\n\n" .
-                                "首次提现前，请先在个人中心-补全资料处，补齐提现资料，即可进行提现\n\n" .
-                                "另外建议您点击个人中心-绑定淘宝，绑定常购物的淘宝账号，即可自动跟踪订单，无需返回公众号填写订单号哦";
-                        case 'Contact':
-                            return "wxid_ts6hzzyc160x22\n" . "您可以复制上方微信号，添加客服微信咨询";
-                        case 'Supplement':
-                            $url = config('config.apiUrl') . "/reg/" . $openid . "?username=" . $user->username . "&nickname=" . $user->nickname . "&alipay=" . $user->alipay_id;
-                            if ($user->username != null && $user->username != "") {
-                                return "您已填写过资料，<a href=\"" . $url . "\">请点此进行资料修改</a>";
-                            } else {
-                                return "<a href=\"" . $url . "\">请点此进行资料补全</a>";
-                            }
-
-
-                        case 'Manual':
-                            return "注册请求已提交，请等待人工为您处理。";
-                        case 'Bind':
-                            $url = config('config.apiUrl') . "/bind/" . $openid;
-                            if($user->special_id == null && $user->special_id == ""){
-                                return "<a href=\"" . $url . "\">请点此进行淘宝绑定</a>，如提示复制到浏览器打开，可在浏览器继续完成绑定";
-                            }else{
-                                return "您已经绑定过淘宝账号了，<a href=\"" . $url . "\">点此进行更换或重新绑定</a>，如提示复制到浏览器打开，可在浏览器继续完成绑定";
-                            }
-
-                        case 'Receive':
-                            if ($user->alipay_id != null && $user->alipay_id != "") {
-                                return "您还没有绑定提现账号，请在资料补全功能下填写";
-                            } else {
-                                return "提现请求发起成功，提现成功0元，将在24小时发送到您的支付宝账号：" . $user->alipay_id;
-                            }
-                        case 'Order':
-                            $url = config('config.apiUrl') . "/order/" . $openid;
-                            return "<a href=\"" . $url . "\">点击此处快速查询订单</a>";
-                        case 'Price':
-                            return "您当前可提现余额0元，待结算金额0元，待结算金额将在您的订单确认收货第10天进行结算。";
-                        default:
-                            return "您的请求暂时无法处理，如有疑问请联系客服";
-                    }
-
+                    return '收到事件消息，系统暂时不处理该业务！请进行其他操作';
+                    break;
                 case 'text':
                     $content = $message['Content'];
 
                     //通过用户对象检测空信息，如有信息为空，则依次使用$content填充
                     //if()
-                    if (stristr($content, '关键词') != false) {
+
+
+
+                    if (stristr($content,'关键词') != false) {
                         return
-                            "1、发送包含【淘口令】的内容，系统将自动转链为返利链接回复\n" .
-                            "2、发送您的【淘宝订单号】或【饿了么订单号】，系统将自动匹配您的下单信息并记录\n" .
-                            "3、发送【订单查询】，可查询已绑定订单的状态\n" .
-                            "4、发送【余额查询】，可查询待结算和可结算的返利金额\n" .
+                            "1、发送包含【淘口令】的内容，系统将自动转链为返利链接回复\n".
+                            "2、发送您的【淘宝订单号】或【饿了么订单号】，系统将自动匹配您的下单信息并记录\n".
+                            "3、发送【订单查询】，可查询已绑定订单的状态\n".
+                            "4、发送【余额查询】，可查询待结算和可结算的返利金额\n".
                             "5、发送【提现】，可将可结算金额提现至支付宝";
-                    } else if (stristr($content, '订单查询') != false) {
+                    }else if(stristr($content,'订单查询') != false){
                         //调用订单查询函数，查询指定openid下的最近订单
-                        return "进入订单查询模块,openid:" . $openid;
-                    } else if (stristr($content, '余额查询') != false) {
+                        return "进入订单查询模块,openid:".$openid ;
+                    }else if(stristr($content,'余额查询') != false){
                         //调用余额查询函数，查询指定openid下的余额状态
-                        return "进入余额查询模块,openid:" . $openid;
-                    } else if (stristr($content, '提现') != false) {
+                        return "进入余额查询模块,openid:".$openid ;
+                    }else if(stristr($content,'提现') != false){
                         //调用提现函数，清除对应openid下的可结算金额，记录提现后台等待人工返现
-                        return "进入提现模块,openid:" . $openid;
-                    } else if (stristr($content, '注册') != false) {
+                        return "进入提现模块,openid:".$openid ;
+                    }else if(stristr($content,'注册') != false){
                         //调用提现函数，清除对应openid下的可结算金额，记录提现后台等待人工返现
-                        $url = config('config.apiUrl') . "/reg/" . $openid;
-                        return "<a href=\"" . $url . "\">请点此进行注册</a>";
-                    } else if (preg_match("/^\d{17,20}$/", $content)) {
+                        $url = config('config.apiUrl')."/reg/".$openid;
+                        return "<a href=\"".$url."\">请点此进行注册</a>" ;
+                    }else if(preg_match("/^\d{17,20}$/",$content)){
                         //调用淘宝联盟订单查询函数，查询对应订单号，如结果不为false，则存入用户订单列表，并返回预估返现信息
-                        return "进入订单绑定模块,openid:" . $openid;
-                    } else if (stristr($content, 'kkb5201314') != false) {
-                        $this->setButton();
-                        return "设置菜单";
-                    } else {
+                        return "进入订单绑定模块,openid:".$openid ;
+                    }else{
                         //调用大淘客接口对所收到的信息进行解析转链，并将优惠券、返利信息返回
-                        return app(TaokeController::class)->parse($user, $content);
+                        return app(TaokeController::class) -> parse($openid,$content);
                     }
                     break;
                 default:
@@ -180,12 +71,6 @@ class WeChatController extends Controller
             }
         });
         return $app->server->serve();
-    }
-
-    public function setButton()
-    {
-        $app = app('wechat.official_account');
-        $app->menu->create($this->buttons);
     }
 }
 
