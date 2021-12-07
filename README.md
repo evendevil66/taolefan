ff6471784da2c1dffce5c29e53e7a7a8ea69ef42
zhangjiaqi <evendevil66@gmail.com>
2021-12-08 02:10:10 +0800

    update

--- README.md
+++ README.md
@@ -15,4 +15,41 @@
 
 ## 已实现功能
 1、公众号转链，用户发送原始或其他淘客的淘口令到公众号，后台转链并计算出返利金额、获取优惠券信息返回给用户。  
-2、注册功能，用户通过公众号注册，绑定微信openID（微信唯一标识）和淘宝special_id（淘宝会员运营唯一标识）。  
+2、注册功能，关注即可完成注册，绑定微信openID（微信唯一标识）。用户可以通过公众号菜单快速补全提现账户信息，还可以绑定淘宝账号，获取special_id（淘宝会员运营唯一标识）
+
+## 部署方法
+环境要求：PHP > 7 && PHP < 8  ｜ MySQL  
+
+下载或clone项目代码到所需环境  
+````shell script
+git clone https://github.com/evendevil66/taolefan.git
+````
+执行Composer命令安装依赖包及自动加载  
+````shell script
+composer install
+composer dump-auto
+````
+复制.env.example文件为.env
+````shell script
+cp .env.example .env
+````
+修改.env中的数据库配置
+````shell script
+DB_CONNECTION=mysql  #默认使用mysql请勿修改 可支持MariaDB
+DB_HOST=127.0.0.1  #数据库连接地址
+DB_PORT=3306  #数据库连接端口
+DB_DATABASE=taolefan #数据库名
+DB_USERNAME=root  #数据库用户名
+DB_PASSWORD=  #数据库密码
+````
+
+修改/config/wechat.php配置 根据微信公众平台内容修改
+````php
+'app_id' => env('WECHAT_OFFICIAL_ACCOUNT_APPID', 'appid'),
+'secret' => env('WECHAT_OFFICIAL_ACCOUNT_SECRET', 'secret'), 
+'token' => env('WECHAT_OFFICIAL_ACCOUNT_TOKEN', 'token'),
+'aes_key' => env('WECHAT_OFFICIAL_ACCOUNT_AES_KEY', 'aeskey')
+````
+修改/config/config.php配置
+
+
--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use EasyWeChat\OfficialAccount\Application;
@@ -10,17 +11,42 @@ class WeChatController extends Controller
 {
     protected $buttons = [
         [
-            "type" => "click",
             "name" => "使用教程",
-            "key"  => "Course"
+            "sub_button" => [
+                [
+                    "type" => "click",
+                    "name" => "使用教程",
+                    "key" => "Course"
+                ],
+                [
+                    "type" => "click",
+                    "name" => "联系客服",
+                    "key" => "Contact"
+                ],
+            ],
         ],
         [
-            "type" => "click",
-            "name" => "联系客服",
-            "key"  => "Contact"
+            "name" => "饭粒中心",
+            "sub_button" => [
+                [
+                    "type" => "click",
+                    "name" => "订单查询",
+                    "key" => "Order"
+                ],
+                [
+                    "type" => "click",
+                    "name" => "余额查询",
+                    "key" => "Price"
+                ],
+                [
+                    "type" => "click",
+                    "name" => "提现",
+                    "key" => "Receive"
+                ],
+            ],
         ],
         [
-            "name"       => "个人中心",
+            "name" => "个人中心",
             "sub_button" => [
                 [
                     "type" => "click",
@@ -36,11 +62,6 @@ class WeChatController extends Controller
                     "type" => "click",
                     "name" => "绑定淘宝",
                     "key" => "Bind"
-                ],
-                [
-                    "type" => "click",
-                    "name" => "提现",
-                    "key" => "Receive"
                 ],
             ],
         ],
@@ -58,51 +79,58 @@ class WeChatController extends Controller
         $app->server->push(function ($message) {
             $openid = $message['FromUserName'];
             //访问数据库获取openid对应用户信息并返回用户对象
-            $user = app(Users::class) -> getUserById($openid);
+            $user = app(Users::class)->getUserById($openid);
             switch ($message['MsgType']) {
                 case 'event':
-                    if($message['Event'] == 'subscribe'){
-                        if($user == null){
-                            $reg = app(Users::class) -> userRegistration($openid);
-                            if($reg==1){
-                                return "欢迎关注".config('config.name')."，已为您自动注册成功。"."\n建议您点击下方个人中心-资料补全，更新资料后开始省钱之旅";
-                            }else{
-                                return "欢迎关注".config('config.name')."您的自动注册请求失败，可能系统出现异常，您可以点击下方个人中心-快速注册进行手动注册";
+                    if ($message['Event'] == 'subscribe') {
+                        if ($user == null) {
+                            $reg = app(Users::class)->userRegistration($openid);
+                            if ($reg == 1) {
+                                return "欢迎关注" . config('config.name') . "，已为您自动注册成功。" . "\n建议您点击下方个人中心-资料补全，更新资料后开始省钱之旅";
+                            } else {
+                                return "欢迎关注" . config('config.name') . "您的自动注册请求失败，可能系统出现异常，您可以点击下方个人中心-快速注册进行手动注册";
                             }
-                        }else{
-                            return "欢迎回到".config('config.name')."，已为您恢复账号。"."\n您可以点击下方个人中心-资料补全，确保资料完整即可开始省钱之旅";
+                        } else {
+                            return "欢迎回到" . config('config.name') . "，已为您恢复账号。" . "\n您可以点击下方个人中心-资料补全，确保资料完整即可开始省钱之旅";
                         }
                     }
 
-                    if($message['EventKey'] == 'Course'){
-                        return
-                            "您可以直接将想购买的商品淘口令发送到这里，系统即可自动转链哦，复制转链后的淘口令打开淘宝继续购物，即可享受优惠和饭粒啦！\n\n".
-                            "首次提现前，请先在个人中心-补全资料处，补齐提现资料，即可进行提现\n\n".
-                            "另外建议您点击个人中心-绑定淘宝，绑定常购物的淘宝账号，即可自动跟踪订单，无需返回公众号填写订单号哦";
-                    }else{
-                        switch ($message['EventKey']){
-                            case 'Contact':
-                                return "wxid_ts6hzzyc160x22\n"."您可以复制上方微信号，添加客服微信咨询";
-                            case 'Supplement':
-                                $user = app(Users::class) -> getUserById($openid);
-                                $url = config('config.apiUrl')."/reg/".$openid."?username=".$user->username."&nickname=".$user->nickname."&alipay=".$user->alipay_id;
-                                if($user->username != null ){
-                                    return "<a href=\"".$url."\">请点此进行资料补全修改</a>" ;
-                                }else{
-                                    return "<a href=\"".$url."\">请点此进行资料补全</a>" ;
-                                }
+                    $user = app(Users::class)->getUserById($openid);
+                    switch ($message['EventKey']) {
+                        case 'Course':
+                            return
+                                "您可以直接将想购买的商品淘口令发送到这里，系统即可自动转链哦，复制转链后的淘口令打开淘宝继续购物，即可享受优惠和饭粒啦！\n\n" .
+                                "首次提现前，请先在个人中心-补全资料处，补齐提现资料，即可进行提现\n\n" .
+                                "另外建议您点击个人中心-绑定淘宝，绑定常购物的淘宝账号，即可自动跟踪订单，无需返回公众号填写订单号哦";
+                        case 'Contact':
+                            return "wxid_ts6hzzyc160x22\n" . "您可以复制上方微信号，添加客服微信咨询";
+                        case 'Supplement':
+                            $url = config('config.apiUrl') . "/reg/" . $openid . "?username=" . $user->username . "&nickname=" . $user->nickname . "&alipay=" . $user->alipay_id;
+                            if ($user->username != null && $user->username != "") {
+                                return "您已填写过资料，<a href=\"" . $url . "\">请点此进行资料修改</a>";
+                            } else {
+                                return "<a href=\"" . $url . "\">请点此进行资料补全</a>";
+                            }
 
 
-                            case 'Manual':
-                                return "注册请求已提交，请等待人工为您处理。";
-                            case 'Bind':
-                                $url = config('config.apiUrl')."/bind/".$openid;
-                                return "<a href=\"".$url."\">请点此进行淘宝绑定</a>" ;
-                            case 'Receive':
+                        case 'Manual':
+                            return "注册请求已提交，请等待人工为您处理。";
+                        case 'Bind':
+                            $url = config('config.apiUrl') . "/bind/" . $openid;
+                            return "<a href=\"" . $url . "\">请点此进行淘宝绑定</a>";
+                        case 'Receive':
+                            if ($user->alipay_id != null && $user->alipay_id != "") {
                                 return "您还没有绑定提现账号，请在资料补全功能下填写";
-                            default:
-                                return "您的请求暂时无法处理，如有疑问请联系客服";
-                        }
+                            } else {
+                                return "提现请求发起成功，提现成功0元，将在24小时发送到您的支付宝账号：" . $user->alipay_id;
+                            }
+                        case 'Order':
+                            $url = config('config.apiUrl') . "/order/" . $openid;
+                            return "<a href=\"" . $url . "\">点击此处快速查询订单</a>";
+                        case 'Price':
+                            return "您当前可提现余额0元，待结算金额0元，待结算金额将在您的订单确认收货第10天进行结算。";
+                        default:
+                            return "您的请求暂时无法处理，如有疑问请联系客服";
                     }
 
                 case 'text':
@@ -110,35 +138,35 @@ class WeChatController extends Controller
 
                     //通过用户对象检测空信息，如有信息为空，则依次使用$content填充
                     //if()
-                    if (stristr($content,'关键词') != false) {
+                    if (stristr($content, '关键词') != false) {
                         return
-                            "1、发送包含【淘口令】的内容，系统将自动转链为返利链接回复\n".
-                            "2、发送您的【淘宝订单号】或【饿了么订单号】，系统将自动匹配您的下单信息并记录\n".
-                            "3、发送【订单查询】，可查询已绑定订单的状态\n".
-                            "4、发送【余额查询】，可查询待结算和可结算的返利金额\n".
+                            "1、发送包含【淘口令】的内容，系统将自动转链为返利链接回复\n" .
+                            "2、发送您的【淘宝订单号】或【饿了么订单号】，系统将自动匹配您的下单信息并记录\n" .
+                            "3、发送【订单查询】，可查询已绑定订单的状态\n" .
+                            "4、发送【余额查询】，可查询待结算和可结算的返利金额\n" .
                             "5、发送【提现】，可将可结算金额提现至支付宝";
-                    }else if(stristr($content,'订单查询') != false){
+                    } else if (stristr($content, '订单查询') != false) {
                         //调用订单查询函数，查询指定openid下的最近订单
-                        return "进入订单查询模块,openid:".$openid ;
-                    }else if(stristr($content,'余额查询') != false){
+                        return "进入订单查询模块,openid:" . $openid;
+                    } else if (stristr($content, '余额查询') != false) {
                         //调用余额查询函数，查询指定openid下的余额状态
-                        return "进入余额查询模块,openid:".$openid ;
-                    }else if(stristr($content,'提现') != false){
+                        return "进入余额查询模块,openid:" . $openid;
+                    } else if (stristr($content, '提现') != false) {
                         //调用提现函数，清除对应openid下的可结算金额，记录提现后台等待人工返现
-                        return "进入提现模块,openid:".$openid ;
-                    }else if(stristr($content,'注册') != false){
+                        return "进入提现模块,openid:" . $openid;
+                    } else if (stristr($content, '注册') != false) {
                         //调用提现函数，清除对应openid下的可结算金额，记录提现后台等待人工返现
-                        $url = config('config.apiUrl')."/reg/".$openid;
-                        return "<a href=\"".$url."\">请点此进行注册</a>" ;
-                    }else if(preg_match("/^\d{17,20}$/",$content)){
+                        $url = config('config.apiUrl') . "/reg/" . $openid;
+                        return "<a href=\"" . $url . "\">请点此进行注册</a>";
+                    } else if (preg_match("/^\d{17,20}$/", $content)) {
                         //调用淘宝联盟订单查询函数，查询对应订单号，如结果不为false，则存入用户订单列表，并返回预估返现信息
-                        return "进入订单绑定模块,openid:".$openid ;
-                    }else if(stristr($content,'kkb5201314') != false){
+                        return "进入订单绑定模块,openid:" . $openid;
+                    } else if (stristr($content, 'kkb5201314') != false) {
                         $this->setButton();
-                        return "设置菜单" ;
-                    }else{
+                        return "设置菜单";
+                    } else {
                         //调用大淘客接口对所收到的信息进行解析转链，并将优惠券、返利信息返回
-                        return app(TaokeController::class) -> parse($openid,$content);
+                        return app(TaokeController::class)->parse($openid, $content);
                     }
                     break;
                 default:
