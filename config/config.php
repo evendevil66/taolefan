03f4c0ae519fb666588d32fee221ff05690e91d4
evendevil66 <50394633+evendevil66@users.noreply.github.com>
2021-12-08 20:48:15 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,12 +5,12 @@ return [
     'name' => "猫乐饭", //产品名称 会反应在用户交互等场景
     'url' => "https://fanli.mttgo.com", //站点url
     'apiUrl' => "https://wechat.mttgo.com", //站点url
-    'dtkAppKey' => "5f61c53050d71", //大淘客appKey 使用大淘客接口快速解析商品信息
-    'dtkAppSecret' => "db82913bfba901fce31c89b264adabcf", //大淘客AppSecret
-    'aliAppKey' => "28177009", //淘宝联盟AppKey
-    'aliAppSecret' => "c6078ce94ea6e7f96445a0e1d0b0a8bd", //淘宝联盟AppSecret
-    'pubpid' => 'mm_129401632_1124900049_111812950261', //公用PID
-    'relationId'=>'2677223535', //渠道ID
-    'inviter_code'=>'U5XG3C' //会员管理邀请码
+    'dtkAppKey' => "******", //大淘客appKey 使用大淘客接口快速解析商品信息
+    'dtkAppSecret' => "******", //大淘客AppSecret
+    'aliAppKey' => "******", //淘宝联盟AppKey
+    'aliAppSecret' => "******", //淘宝联盟AppSecret
+    'pubpid' => '******', //公用PID
+    'relationId'=>'******', //渠道ID
+    'inviter_code'=>'******' //会员管理邀请码
 ];
 
